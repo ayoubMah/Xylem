c94b4a80d9e5c3f55468abd7607115586a4e6ebf
ayoubMah <ayoubelmahjouby9@gmail.com>
2026-05-17 01:59:08 +0200

    test growth

--- src/main.go
+++ src/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	// stage1
 	store := NewKVStore()
 
 	store.Set("name", []byte("Aub"))
@@ -20,6 +22,29 @@ func main() {
 
 	store.Delete("name")
 	if _, ok := store.Get("name"); !ok {
-		fmt.Printf("name deleted!")
+		fmt.Printf("name deleted!\n")
+	}
+
+	// test the measureGrowth
+	measureGrowth()
+}
+
+func measureGrowth() {
+	store := NewKVStore() // change it with make(map[string][]byte, 10000) and test again and change the line 42 to 43
+
+	threshold := 2 * time.Microsecond
+
+	for i := range 10_000 {
+		key := fmt.Sprintf("key-%d", i)
+		value := []byte("v")
+
+		start := time.Now()
+		store.Set(key, value)
+		// store[key] = value
+		elapsed := time.Since(start)
+
+		if elapsed > threshold {
+			fmt.Printf("spike at key #%d: %v\n", i, elapsed)
+		}
 	}
 }
