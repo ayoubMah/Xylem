570c32bbff3706aa55b8b43539bfa7fccd23d6d3
ayoubMah <ayoubelmahjouby9@gmail.com>
2026-05-17 13:47:54 +0200

    feat: thread-safe concurrency model using an RWMutex

--- src/store.go
+++ src/store.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"sync"
+)
+
 type KVStore struct {
+	mu   sync.RWMutex
 	data map[string][]byte
 }
 
@@ -12,16 +17,22 @@ func NewKVStore() *KVStore {
 
 func (s *KVStore) Set(key string, value []byte) {
 	// imp
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.data[key] = value
 }
 
 func (s *KVStore) Get(key string) ([]byte, bool) {
 	// imp
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	val, ok := s.data[key]
 	return val, ok
 }
 
 func (s *KVStore) Delete(key string) {
 	// imp
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	delete(s.data, key)
 }
