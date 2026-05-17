2a64de455961adaa9ba46fbf7f3f9fb7a02a807d
ayoubMah <ayoubelmahjouby9@gmail.com>
2026-05-17 04:34:04 +0200

    test simulate traffic

--- src/main.go
+++ src/main.go
@@ -7,26 +7,29 @@ import (
 
 func main() {
 	// stage1
-	store := NewKVStore()
-
-	store.Set("name", []byte("Aub"))
-	store.Set("lang", []byte("Go"))
-
-	if val, ok := store.Get("name"); ok {
-		fmt.Printf("name: %s\n", val)
-	}
-
-	if val, ok := store.Get("lang"); ok {
-		fmt.Printf("lang: %s\n", val)
-	}
-
-	store.Delete("name")
-	if _, ok := store.Get("name"); !ok {
-		fmt.Printf("name deleted!\n")
-	}
+	// store := NewKVStore()
+	//
+	// store.Set("name", []byte("Aub"))
+	// store.Set("lang", []byte("Go"))
+	//
+	// if val, ok := store.Get("name"); ok {
+	// 	fmt.Printf("name: %s\n", val)
+	// }
+	//
+	// if val, ok := store.Get("lang"); ok {
+	// 	fmt.Printf("lang: %s\n", val)
+	// }
+	//
+	// store.Delete("name")
+	// if _, ok := store.Get("name"); !ok {
+	// 	fmt.Printf("name deleted!\n")
+	// }
 
 	// test the measureGrowth
-	measureGrowth()
+	//measureGrowth()
+	//
+	// test the Traffic
+	simulateTraffic()
 }
 
 func measureGrowth() {
@@ -47,4 +50,29 @@ func measureGrowth() {
 			fmt.Printf("spike at key #%d: %v\n", i, elapsed)
 		}
 	}
+}
+
+func simulateTraffic() {
+	store := NewKVStore()
+	store.Set("status", []byte("running"))
+
+	// 1 Writer trying to update the map
+	go func() {
+		for i := 0; i < 1000; i++ {
+			store.Set("status", []byte("updating"))
+		}
+	}()
+
+	// 10 Readers trying to read the map at the same time
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				store.Get("status")
+			}
+		}()
+	}
+
+	// Give the goroutines a second to finish their chaos
+	time.Sleep(1 * time.Second)
+	fmt.Println("Traffic simulation complete!")
 }
